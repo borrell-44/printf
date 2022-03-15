5038871395990b5d41a79cecc8f13369d9b11e5b
griseliz <griseliz.r.c15@gmail.com>
2022-03-15 19:54:10 +0000

    codes

--- print_functions.c
+++ print_functions.c
@@ -3,53 +3,6 @@
 #include <stdio.h>
 
 /**
- *print_c - print character
- *ch: character to print
- *Return: a character
- */
-
-int print_c (va_list *ch)
-{
-	int i;
-	va_list ch1;
-
-	va_copy(ch1, *ch);
-
-	i = va_arg (ch1, int);
-
-	_putchar(i);
-
-	return (i);
-}
-
-/**
- *print_str - prints a string
- *@s - the string
- *Return: a string
- */
-
-int print_str(va_list *s)
-{
-	char *str;
-	int i;
-	va_list s1;
-
-	va_copy(s1, *s);
-	str = va_arg(s1, char *);
-
-	if (str == NULL)
-		exit(98);
-
-	for (i = 0; _strlen(str); i++)
-	{
-		_putchar(str[i]);
-	}
-	va_end(s1);
-	
-	return (i);
-}
-
-/**
  *print_r - print a reverse string
  *@r: string
  *Return: rev characters
@@ -60,7 +13,7 @@ int print_r (va_list *r)
 	char *s;
 	int i;
 	va_list r1;
-
+	
 	va_copy(r1, *r);
 
 	s = va_arg(r1, char*);
@@ -70,7 +23,7 @@ int print_r (va_list *r)
 				s++;
 	}
 
-	for (i -= 1; i >= 0; i ++)
+	for (i -= 1; i >= 0; i --)
 	{
 		_putchar(s[i]);
 	}
